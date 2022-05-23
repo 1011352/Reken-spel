79d2e9624537dbceb3a3d32109183290e936a9dd
1011352 <1011352@hr.nl>
2022-05-23 10:23:55 +0200

    test

---
+++ src/game.ts
@@ -0,0 +1,14 @@
+import * as PIXI from 'pixi.js'
+
+export class Game {
+    pixi: PIXI.Application;
+    loader: PIXI.Loader;
+
+    constructor() {
+        this.pixi = new PIXI.Application({ width: 800, height: 450 })
+        document.body.appendChild(this.pixi.view)
+        this.loader = new PIXI.Loader()
+    }
+
+
+}
